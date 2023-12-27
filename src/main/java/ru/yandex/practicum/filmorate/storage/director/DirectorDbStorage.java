957e999dcdb3dbc0e9a3f954bcc2553ee00cd5a7
Kim Bogdanov <105812215+KimBogdanov@users.noreply.github.com>
2023-12-27 12:10:12 +0300

    feat: implementation DirectorDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorDbStorage.java
@@ -1,12 +1,15 @@
 package ru.yandex.practicum.filmorate.storage.director;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Director;
 
-import java.util.HashMap;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -26,21 +29,40 @@ public class DirectorDbStorage implements DirectorStorage {
 
     @Override
     public Director getDirectorById(int id) {
-        return null;
+        String sql = "SELECT * " +
+                "FROM DIRECTORS " +
+                "WHERE DIRECTOR_ID = ?";
+        try {
+            return jdbcTemplate.queryForObject(sql, this::directorMapper, id);
+        } catch (DataAccessException e) {
+            throw new NotFoundException("Директор не найден");
+        }
     }
 
     @Override
     public List<Director> getAllDirectors() {
-        return null;
+        String sql = "SELECT * FROM DIRECTORS";
+        return jdbcTemplate.query(sql, this::directorMapper);
     }
 
     @Override
     public Director updateDirector(Director director) {
-        return null;
+        String sql = "UPDATE directors SET DIRECTOR_NAME = ? WHERE DIRECTOR_ID = ?";
+        int result = jdbcTemplate.update(sql, director.getName(), director.getId());
+        if (result != 1) {
+            throw new NotFoundException("Директор не найден");
+        }
+        return director;
     }
 
     @Override
     public int deleteDirectorById(int id) {
-        return 0;
+        String sql = "DELETE FROM DIRECTORS WHERE DIRECTOR_ID = ?";
+        return jdbcTemplate.update(sql, id);
+    }
+
+    private Director directorMapper(ResultSet rs, int rowNum) throws SQLException {
+        return new Director(rs.getInt("director_id"),
+                rs.getString("director_name"));
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/director/DirectorStorage.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface DirectorStorage {
     Director createDirector(Director director);
+
     Director getDirectorById(int id);
+
     List<Director> getAllDirectors();
+
     Director updateDirector(Director director);
+
     int deleteDirectorById(int id);
 }
