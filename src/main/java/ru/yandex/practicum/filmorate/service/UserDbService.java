d8c114ac71e44d2319140a27d4fa27d10897122b
Oleg <emmoler@yandex.ru>
2024-01-09 19:09:05 +0300

    add-remove-endpoint

--- src/main/java/ru/yandex/practicum/filmorate/service/UserDbService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserDbService.java
@@ -26,7 +26,10 @@ public class UserDbService {
     }
 
     public int deleteUser(int id) {
-        return userStorage.deleteUserById(id);
+        if (userStorage.isExistingUser(id))
+            return userStorage.deleteUserById(id);
+        else
+            throw new NotFoundException(String.format("Пользователь %d не может быть найден", id));
     }
 
     public List<User> getUsers() {
@@ -50,7 +53,10 @@ public class UserDbService {
     }
 
     public List<User> getFriends(int id) {
-        return userStorage.getFriendsById(id);
+        if (userStorage.isExistingUser(id))
+            return userStorage.getFriendsById(id);
+        else
+            throw new NotFoundException(String.format("Пользователь %d не может быть найден", id));
     }
 
     public List<User> getCommonFriends(int id, int otherId) {
